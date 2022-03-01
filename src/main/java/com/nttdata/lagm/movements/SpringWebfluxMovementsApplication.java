287fbf62b72f627f8c26a762448ccbfd50115825
Luis Guisado <lguisadom@gmail.com>
2022-03-01 11:19:20 -0500

    Begin

---
+++ src/main/java/com/nttdata/lagm/movements/SpringWebfluxMovementsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.movements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxMovementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxMovementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/lagm/movements/SpringWebfluxMovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.movements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
