fff2398f7cbba7bc9dbb457ac687781111324dd4
Luis Guisado <lguisadom@gmail.com>
2022-03-03 15:58:01 -0500

    Adding Eureka Client

--- src/main/java/com/nttdata/lagm/movements/SpringWebfluxMovementsApplication.java
+++ src/main/java/com/nttdata/lagm/movements/SpringWebfluxMovementsApplication.java
@@ -2,7 +2,9 @@ package com.nttdata.lagm.movements;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringWebfluxMovementsApplication {
 
